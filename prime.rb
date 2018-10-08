865296a7d76e07421c715b2b80d2f953ac2a397e
Carlo Fernando <ca.fernando415@gmail.com>
2018-10-08 03:28:12 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,4 @@
 # Add  code here!
 def prime?(num)
-  if num > 0
-    if num == 2 
-      true
-    else
-      (num % (Math.sqrt(num).ceil)) > 0 ? true : false
-    end
-  else
-    false
-  end
+  if (num < 1 )
 end
